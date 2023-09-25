3bbb436f892eae8fb49360851a182a7a0e99b47a
Andrew Monteith <339531824@GoTVDSB.ca>
2023-09-25 10:57:11 -0400

    started int list

--- Program.cs
+++ Program.cs
@@ -10,7 +10,48 @@ namespace Lists_Programming_Assignment
     {
         static void Main(string[] args)
         {
-
+            while (true)
+            {
+                string menuOption;
+                Console.WriteLine("Press 1 for Integer List");
+                Console.WriteLine("Press 2 for String List");
+                Console.WriteLine("Press Q for Quit");
+                menuOption = Console.ReadLine();
+                if (menuOption == "1")
+                {
+                    IntegerList();
+                }
+                else if (menuOption == "2")
+                {
+                    
+                }
+                else if (menuOption == "Q" || menuOption == "q")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+        public static void IntegerList()
+        {
+            Random generator = new Random();
+            List <int> numbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                if (i < 24)
+                {
+                    numbers.Add(generator.Next(10, 21));
+                    Console.Write(numbers[i] + ", ");
+                }
+                else
+                {
+                    numbers.Add(generator.Next(10, 21));
+                    Console.WriteLine(numbers[i]);
+                }
+            }
         }
     }
 }
